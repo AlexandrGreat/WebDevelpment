39e35ccc6aa78923c2e40d2d0b8df50ed5ba1f9d
AlexandrGreat <109344446+AlexandrGreat@users.noreply.github.com>
2023-09-19 10:55:29 +0300

    Delete components/titleComponent.jsx

--- components/titleComponent.jsx
+++
@@ -1,8 +0,0 @@
-import { Component } from "react";
-
-class TitleComponent extends Component{
-    render(){
-        return(<h1>This is component from other class!</h1>);
-    }
-}
-export default TitleComponent;
