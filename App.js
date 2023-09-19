7145329a8889d825902f48d75411e13ad7856bc9
AlexandrGreat <109344446+AlexandrGreat@users.noreply.github.com>
2023-09-19 10:53:52 +0300

    Add files via upload

--- App.js
+++ App.js
@@ -1,33 +1,18 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
-import TitleComponent from './components/titleComponent';
-import TableComponent from './components/tableComponent';
+import {Body,Header,Menu,Footer} from './components/index';
 
-function App() {
+class App extends React.Component {
+  render(){
   return (
     <div className="App">
-      <TableComponent />
-      <TitleComponent />
-      <StandartViewComponent />
+      <Header />
+      <Menu/>
+      <Body/>
+      <Footer/>
     </div>
   );
+  }
 }
-
-const StandartViewComponent =()=>{
-  return(<header className="App-header">
-  <img src={logo} className="App-logo" alt="logo" />
-  <p>
-    Edit <code>src/App.js</code> and save to reload.
-  </p>
-  <a
-    className="App-link"
-    href="https://reactjs.org"
-    target="_blank"
-    rel="noopener noreferrer"
-  >
-    Learn React
-  </a>
-</header>)
-}
-
+export const poductList=['Glock 19','Colt 1911','Desert Eagle','Thompson 1928','M 16','Galil','Tec 9'];
 export default App;
---
+++ components/index.js
@@ -0,0 +1,6 @@
+import Body from "./body";
+import Header from "./header";
+import Menu from "./menu";
+import Footer from "./footer";
+
+export {Body, Header, Menu, Footer};
